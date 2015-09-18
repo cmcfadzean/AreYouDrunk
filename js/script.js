2dbbf69b7c1869f736eb18e9958fa45bb0bae6e1
James McFadzean <jmcfadzean@alttechsolutions.com>
2015-09-18 10:13:27 -0700

    Building out the Angular JS MVC

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// script.js
+
+    // create the module and name it scotchApp
+    var angularApp = angular.module('angularApp', ['ngRoute']);
+
+    angularApp.config(function($routeProvider){
+    	$routeProvider
+
+	    	//Route for Home page
+	    	.when('/', {
+	    		templateUrl : 'pages/home.html',
+	    		controller  : 'mainController'
+	    	})
+
+	    	//Route for About page
+	    	.when('/info', {
+	    		templateUrl : 'pages/info.html',
+	    		controller  : 'aboutController'
+	    	})
+
+    });
+
+    // create the controller and inject Angular's $scope
+    angularApp.controller('mainController', function($scope) {
+
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+        $scope.user = [{
+            "id": "1",
+            "first": "Jason",
+            "last": "Stathem",
+            "marketValue": "",
+            "stocks": [
+                "Dell",
+                "McDonalds",
+                "Shell Old",
+                "Apple"
+              ]
+        }, ];
+    });
+
+    angularApp.controller('infoController', function($scope) {
+    	$scope.message = 'This is the Info page.';
+    });
